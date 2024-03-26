cccd4d163219cb3727ca96c7caa33071c9990a9f
Константин Валавин <valavin.kostya@outlook.com>
2024-03-26 09:48:02 +0800

    Добавьте файлы проекта.

---
+++ AutoShop/List/Autoriz/LoginForm.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace AutoShop.List.Autoriz
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges33 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges34 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges25 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges26 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges27 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges28 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges29 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges30 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges31 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges32 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(components);
+            guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            loginBtn = new Guna.UI2.WinForms.Guna2Button();
+            label2 = new Label();
+            label1 = new Label();
+            guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            guna2Panel3 = new Guna.UI2.WinForms.Guna2Panel();
+            passwordBox = new Guna.UI2.WinForms.Guna2TextBox();
+            emailBox = new Guna.UI2.WinForms.Guna2TextBox();
+            registrationBtn = new Guna.UI2.WinForms.Guna2Button();
+            nameBox = new Guna.UI2.WinForms.Guna2TextBox();
+            googleBtn = new Guna.UI2.WinForms.Guna2CircleButton();
+            label3 = new Label();
+            guna2Panel1.SuspendLayout();
+            guna2Panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            guna2BorderlessForm1.BorderRadius = 25;
+            guna2BorderlessForm1.ContainerControl = this;
+            guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2Panel1
+            // 
+            guna2Panel1.BackColor = Color.FromArgb(58, 179, 153);
+            guna2Panel1.BorderRadius = 25;
+            guna2Panel1.Controls.Add(loginBtn);
+            guna2Panel1.Controls.Add(label2);
+            guna2Panel1.Controls.Add(label1);
+            guna2Panel1.CustomizableEdges = customizableEdges20;
+            guna2Panel1.Dock = DockStyle.Left;
+            guna2Panel1.Location = new Point(0, 0);
+            guna2Panel1.Name = "guna2Panel1";
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges21;
+            guna2Panel1.Size = new Size(444, 768);
+            guna2Panel1.TabIndex = 0;
+            // 
+            // loginBtn
+            // 
+            loginBtn.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            loginBtn.BorderColor = Color.White;
+            loginBtn.BorderRadius = 35;
+            loginBtn.BorderThickness = 3;
+            loginBtn.CustomizableEdges = customizableEdges18;
+            loginBtn.DisabledState.BorderColor = Color.DarkGray;
+            loginBtn.DisabledState.CustomBorderColor = Color.DarkGray;
+            loginBtn.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            loginBtn.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            loginBtn.FillColor = Color.Empty;
+            loginBtn.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            loginBtn.ForeColor = Color.White;
+            loginBtn.Location = new Point(98, 449);
+            loginBtn.Name = "loginBtn";
+            loginBtn.ShadowDecoration.CustomizableEdges = customizableEdges19;
+            loginBtn.Size = new Size(249, 73);
+            loginBtn.TabIndex = 2;
+            loginBtn.Text = "ВОЙТИ";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(6, 318);
+            label2.Name = "label2";
+            label2.Size = new Size(432, 128);
+            label2.TabIndex = 1;
+            label2.Text = "Чтобы поддерживать с нами связь, пожалуйста войдите в систему,\r\nуказав свои личные данные";
+            label2.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 30F);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(41, 238);
+            label1.Name = "label1";
+            label1.Size = new Size(363, 67);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome Back!";
+            // 
+            // guna2Panel2
+            // 
+            guna2Panel2.BorderRadius = 25;
+            guna2Panel2.CustomizableEdges = customizableEdges22;
+            guna2Panel2.Dock = DockStyle.Top;
+            guna2Panel2.Location = new Point(444, 0);
+            guna2Panel2.Name = "guna2Panel2";
+            guna2Panel2.ShadowDecoration.CustomizableEdges = customizableEdges23;
+            guna2Panel2.Size = new Size(854, 68);
+            guna2Panel2.TabIndex = 1;
+            // 
+            // guna2Panel3
+            // 
+            guna2Panel3.BackColor = Color.Transparent;
+            guna2Panel3.Controls.Add(passwordBox);
+            guna2Panel3.Controls.Add(emailBox);
+            guna2Panel3.Controls.Add(registrationBtn);
+            guna2Panel3.Controls.Add(nameBox);
+            guna2Panel3.Controls.Add(googleBtn);
+            guna2Panel3.Controls.Add(label3);
+            guna2Panel3.CustomizableEdges = customizableEdges33;
+            guna2Panel3.Dock = DockStyle.Fill;
+            guna2Panel3.Location = new Point(444, 68);
+            guna2Panel3.Name = "guna2Panel3";
+            guna2Panel3.ShadowDecoration.CustomizableEdges = customizableEdges34;
+            guna2Panel3.Size = new Size(854, 700);
+            guna2Panel3.TabIndex = 2;
+            // 
+            // passwordBox
+            // 
+            passwordBox.BorderColor = Color.Black;
+            passwordBox.BorderRadius = 25;
+            passwordBox.CustomizableEdges = customizableEdges24;
+            passwordBox.DefaultText = "Password";
+            passwordBox.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            passwordBox.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            passwordBox.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            passwordBox.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            passwordBox.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            passwordBox.Font = new Font("Segoe UI", 16F);
+            passwordBox.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            passwordBox.IconLeft = Properties.Resources.free_icon_password_1000966;
+            passwordBox.IconLeftOffset = new Point(5, 0);
+            passwordBox.IconLeftSize = new Size(50, 50);
+            passwordBox.Location = new Point(239, 452);
+            passwordBox.Margin = new Padding(7);
+            passwordBox.Name = "passwordBox";
+            passwordBox.PasswordChar = '\0';
+            passwordBox.PlaceholderText = "";
+            passwordBox.SelectedText = "";
+            passwordBox.ShadowDecoration.CustomizableEdges = customizableEdges25;
+            passwordBox.Size = new Size(371, 73);
+            passwordBox.TabIndex = 8;
+            // 
+            // emailBox
+            // 
+            emailBox.BorderColor = Color.Black;
+            emailBox.BorderRadius = 25;
+            emailBox.CustomizableEdges = customizableEdges26;
+            emailBox.DefaultText = "Email";
+            emailBox.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            emailBox.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            emailBox.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            emailBox.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            emailBox.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            emailBox.Font = new Font("Segoe UI", 16F);
+            emailBox.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            emailBox.IconLeft = Properties.Resources.free_icon_email_envelope_12439139;
+            emailBox.IconLeftOffset = new Point(5, 0);
+            emailBox.IconLeftSize = new Size(47, 47);
+            emailBox.Location = new Point(239, 365);
+            emailBox.Margin = new Padding(7);
+            emailBox.Name = "emailBox";
+            emailBox.PasswordChar = '\0';
+            emailBox.PlaceholderText = "";
+            emailBox.SelectedText = "";
+            emailBox.ShadowDecoration.CustomizableEdges = customizableEdges27;
+            emailBox.Size = new Size(371, 73);
+            emailBox.TabIndex = 7;
+            // 
+            // registrationBtn
+            // 
+            registrationBtn.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            registrationBtn.BackColor = Color.White;
+            registrationBtn.BorderColor = Color.FromArgb(193, 193, 193);
+            registrationBtn.BorderRadius = 35;
+            registrationBtn.CustomBorderColor = Color.Red;
+            registrationBtn.CustomizableEdges = customizableEdges28;
+            registrationBtn.DisabledState.BorderColor = Color.DarkGray;
+            registrationBtn.DisabledState.CustomBorderColor = Color.DarkGray;
+            registrationBtn.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            registrationBtn.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            registrationBtn.FillColor = Color.FromArgb(58, 179, 153);
+            registrationBtn.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            registrationBtn.ForeColor = Color.White;
+            registrationBtn.Location = new Point(303, 560);
+            registrationBtn.Name = "registrationBtn";
+            registrationBtn.ShadowDecoration.CustomizableEdges = customizableEdges29;
+            registrationBtn.Size = new Size(249, 73);
+            registrationBtn.TabIndex = 6;
+            registrationBtn.Text = "Регистрация";
+            registrationBtn.Click += registration_Click;
+            // 
+            // nameBox
+            // 
+            nameBox.BorderColor = Color.Black;
+            nameBox.BorderRadius = 25;
+            nameBox.CustomizableEdges = customizableEdges30;
+            nameBox.DefaultText = "Name";
+            nameBox.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            nameBox.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            nameBox.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            nameBox.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            nameBox.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            nameBox.Font = new Font("Segoe UI", 16F);
+            nameBox.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            nameBox.IconLeft = Properties.Resources.free_icon_user_account_9372916;
+            nameBox.IconLeftOffset = new Point(5, 0);
+            nameBox.IconLeftSize = new Size(50, 50);
+            nameBox.Location = new Point(239, 278);
+            nameBox.Margin = new Padding(7);
+            nameBox.Name = "nameBox";
+            nameBox.PasswordChar = '\0';
+            nameBox.PlaceholderText = "";
+            nameBox.SelectedText = "";
+            nameBox.ShadowDecoration.CustomizableEdges = customizableEdges31;
+            nameBox.Size = new Size(371, 73);
+            nameBox.TabIndex = 3;
+            // 
+            // googleBtn
+            // 
+            googleBtn.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            googleBtn.BackgroundImageLayout = ImageLayout.Zoom;
+            googleBtn.BorderColor = Color.FromArgb(193, 193, 193);
+            googleBtn.BorderThickness = 1;
+            googleBtn.DisabledState.BorderColor = Color.DarkGray;
+            googleBtn.DisabledState.CustomBorderColor = Color.DarkGray;
+            googleBtn.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            googleBtn.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            googleBtn.FillColor = Color.White;
+            googleBtn.Font = new Font("Segoe UI", 9F);
+            googleBtn.ForeColor = Color.White;
+            googleBtn.Location = new Point(402, 196);
+            googleBtn.Name = "googleBtn";
+            googleBtn.ShadowDecoration.CustomizableEdges = customizableEdges32;
+            googleBtn.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            googleBtn.Size = new Size(50, 50);
+            googleBtn.TabIndex = 2;
+            googleBtn.Text = "guna2CircleButton1";
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 30F);
+            label3.ForeColor = Color.FromArgb(58, 179, 153);
+            label3.Location = new Point(229, 115);
+            label3.Name = "label3";
+            label3.Size = new Size(397, 67);
+            label3.TabIndex = 1;
+            label3.Text = "Создать аккаунт";
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1298, 768);
+            Controls.Add(guna2Panel3);
+            Controls.Add(guna2Panel2);
+            Controls.Add(guna2Panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "LoginForm";
+            Text = "LoginForm";
+            guna2Panel1.ResumeLayout(false);
+            guna2Panel1.PerformLayout();
+            guna2Panel3.ResumeLayout(false);
+            guna2Panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private Label label1;
+        private Label label2;
+        private Guna.UI2.WinForms.Guna2Button loginBtn;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel3;
+        private Label label3;
+        private Guna.UI2.WinForms.Guna2TextBox nameBox;
+        private Guna.UI2.WinForms.Guna2CircleButton googleBtn;
+        private Guna.UI2.WinForms.Guna2Button registrationBtn;
+        private Guna.UI2.WinForms.Guna2TextBox passwordBox;
+        private Guna.UI2.WinForms.Guna2TextBox emailBox;
+    }
+}
---
+++ AutoShop/List/Autoriz/LoginForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Drawing.Drawing2D;
+using Microsoft.Win32;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace AutoShop.List.Autoriz
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void registration_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=LAPTOPVALAVIN-K\\SQLEXPRESS;Initial Catalog=AutoShop;Integrated Security=True;";
+            // Проверка, существует ли такой же username в базе данных
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string checkQuery = "SELECT COUNT(*) FROM client WHERE name = @name";
+                using (SqlCommand checkCommand = new SqlCommand(checkQuery, connection))
+                {
+                    checkCommand.Parameters.AddWithValue("@name", nameBox.Text);
+                    int count = (int)checkCommand.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+                        /*
+                        // Если существует запись с таким username, выведите сообщение об ошибке
+                        error.Text = "Имя занято";
+                        Username.ForeColor = Color.Red;
+                        error.ForeColor = Color.Black;
+                        */
+                    }
+                    else
+                    {
+                        // Если такого username нет, продолжайте с добавлением записи в базу данных
+                        string query = "INSERT INTO client (name, password, email) VALUES (@name, @password, @email)";
+
+                        using (SqlCommand command = new SqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@name", nameBox.Text);
+                            command.Parameters.AddWithValue("@email", emailBox.Text);
+                            command.Parameters.AddWithValue("@password", passwordBox.Text);
+
+                            command.ExecuteNonQuery();
+                            /*
+                            notification.Close();
+                            Properties.Settings.Default.succes = "Регистрация успешна!";
+                            Properties.Settings.Default.Save();
+                            Succes succes = new Succes();
+                            succes.Show();
+                            */
+
+                            List.Autoriz.LoginForm login = new List.Autoriz.LoginForm();
+                            this.Close();
+                            login.Show();
+                            /*
+                            error.Text = "Регистрация успешна!";
+                            error.ForeColor = Color.Green;
+                            Password.ForeColor = Color.Black;
+                            Username.ForeColor = Color.Black;
+                            */
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
